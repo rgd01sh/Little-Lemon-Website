84671a36c8e7d693de1f834ec17dc35388e0cf89
RGD S★ <128194619+rgd01sh@users.noreply.github.com>
2024-06-14 06:55:15 +0300

    Delete react-meta-main directory

--- react-meta-main/src/pages/OrderOnline.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const OrderOnline = () => {
-  return (
-    <div>OrderOnline</div>
-  )
-}
--- react-meta-main/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { Reservations } from "./Reservations";
-
-export { About } from "./About";
-export { HomePage } from "./HomePage";
-export { Menu } from "./Menu";
-export { OrderOnline} from "./OrderOnline"
-export { Login} from "./Login"
